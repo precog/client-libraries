48375cff1e59b199f8eef21e866add5a3e1f3bf6
Kris Nuttycombe <kris@hylotech.com>
2011-08-15 14:05:16 -0600

    Made lift-json dep intransitive.

--- scala/project/Build.scala
+++ scala/project/Build.scala
@@ -5,7 +5,7 @@ import AltDependency._
 object ScalaClientBuild extends Build {
   val buildOrganization = "com.reportgrid"
   val buildVersion = "0.3.0"
-  val buildScalaVersion = "2.9.1.RC1"
+  val buildScalaVersion = "2.9.1.RC2"
   
   val blueeyes = GitAltDependency(_: java.io.File, file("../../blueeyes"),     RootProject(uri("git://github.com/jdegoes/blueeyes")))
   val rosetta =  GitAltDependency(_: java.io.File, file("../../rosetta-json"), RootProject(uri("git://github.com/jdegoes/RosettaJson"))) 
@@ -19,7 +19,7 @@ object ScalaClientBuild extends Build {
       libraryDependencies ++= Seq(
         "org.apache.httpcomponents" %  "httpclient"          % "4.1.1",
         "net.databinder"            % "dispatch-http-json_2.9.0-1"  % "0.8.3"   % "provided",
-        "net.liftweb"               % "lift-json_2.9.0-1"           % "2.4-M2"  % "provided",
+        "net.liftweb"               % "lift-json_2.9.0-1"           % "2.4-M2"  % "provided" intransitive(),
         "org.scala-tools.testing"   % "specs_2.9.0-1"               % "1.6.8"   % "test"
       )
     ) dependsOnAlt(blueeyes(base)) dependsOnAlt(rosetta(base)) 
