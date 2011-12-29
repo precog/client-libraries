832a0bff8ad464dcdd353466a8bb96b224b20c44
Kris Nuttycombe <kris@reportgrid.com>
2011-12-29 12:08:59 -0700

    Updated build dependencies on blueeyes and rosetta-json.

--- scala/project/Build.scala
+++ scala/project/Build.scala
@@ -2,45 +2,41 @@ import sbt._
 import Keys._
 
 object ScalaClientBuild extends Build {
-  val buildOrganization = "com.reportgrid"
-  val buildVersion = "0.3.1"
-  val buildScalaVersion = "2.9.1"
-  
   val blueeyesDeps = com.samskivert.condep.Depends( 
-    ("blueeyes",    null, "com.reportgrid" %% "blueeyes"     % "0.5.0-SNAPSHOT")
+    ("blueeyes",    null, "com.reportgrid" %% "blueeyes"     % "0.5.1")
   )
 
   val rosettaDeps =  com.samskivert.condep.Depends(
-    ("RosettaJson", null, "com.reportgrid" %% "rosetta-json" % "0.3.4")
+    ("RosettaJson", null, "com.reportgrid" %% "rosetta-json" % "0.3.5")
   )
 
-  override def projectDefinitions(base: File) = {
-    val client: Project = ((blueeyesDeps.addDeps _) andThen (rosettaDeps.addDeps _))(Project("scala-client", file(".")).settings(
-      organization := buildOrganization,
-      version      := buildVersion,
-      scalaVersion := buildScalaVersion,
+  lazy val client: Project = ((blueeyesDeps.addDeps _) andThen (rosettaDeps.addDeps _)) {
+    Project(id = "scala-client", base = file(".")).settings(
+      organization := "com.reportgrid",
+      version      := "0.3.2",
+      scalaVersion := "2.9.1",
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
-      resolvers += "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots/",
+
       libraryDependencies ++= (blueeyesDeps.libDeps ++ rosettaDeps.libDeps ++ Seq(
         "org.apache.httpcomponents" %  "httpclient"          % "4.1.1",
         "net.databinder"            %% "dispatch-http-json"  % "0.8.5"   % "provided",
         "net.liftweb"               %% "lift-json"           % "2.4-M4"  % "provided" intransitive(),
-        "org.specs2"                %% "specs2"              % "1.7-SNAPSHOT"  % "test"
-      )),
+        "org.specs2"                %% "specs2"              % "1.7-SNAPSHOT"  % "test")),
+      
       resolvers ++= Seq("ReportGrid repo" at                   "http://nexus.reportgrid.com/content/repositories/releases",
-			"ReportGrid snapshot repo" at          "http://nexus.reportgrid.com/content/repositories/snapshots",
-			"ReportGrid public repo" at            "http://nexus.reportgrid.com/content/repositories/public-releases",
-			"ReportGrid public snapshot repo" at   "http://nexus.reportgrid.com/content/repositories/public-snapshots",
-		        "Typesafe repo" at                     "http://repo.typesafe.com/typesafe/releases/"),
+                        "ReportGrid snapshot repo" at          "http://nexus.reportgrid.com/content/repositories/snapshots",
+                        "ReportGrid public repo" at            "http://nexus.reportgrid.com/content/repositories/public-releases",
+                        "ReportGrid public snapshot repo" at   "http://nexus.reportgrid.com/content/repositories/public-snapshots",
+                        "Typesafe repo" at                     "http://repo.typesafe.com/typesafe/releases/"),
+
       publishTo <<= (version) { version: String =>
         val nexus = "http://nexus.reportgrid.com/content/repositories/"
         if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus+"public-snapshots/") 
         else                                   Some("releases"  at nexus+"public-releases/")
       },
+
       credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials")
-    ))
-    
-    client :: Nil
+    )
   }
 }
 
