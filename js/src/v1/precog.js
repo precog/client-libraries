6794722aafccd8b995447b6261d9ec45e600a255
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-03-01 16:08:34 -0700

    first draft of precog js API

---
+++ js/src/v1/precog.js
@@ -0,0 +1,425 @@
+/* Copyright (C) 2012 by Precog, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * No portion of this Software shall be used in any application which does not
+ * use the Precog platform to provide some subset of its functionality.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// JSON parsing & stringification:
+var JSON;
+if(!JSON)JSON={};
+(function(){"use strict";function f(n){return n<10?'0'+n:n;}
+if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(key){return isFinite(this.valueOf())?this.getUTCFullYear()+'-'+
+f(this.getUTCMonth()+1)+'-'+
+f(this.getUTCDate())+'T'+
+f(this.getUTCHours())+':'+
+f(this.getUTCMinutes())+':'+
+f(this.getUTCSeconds())+'Z':null;};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf();};}
+var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;function quote(string){escapable.lastIndex=0;return escapable.test(string)?'"'+string.replace(escapable,function(a){var c=meta[a];return typeof c==='string'?c:'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);})+'"':'"'+string+'"';}
+function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key);}
+if(typeof rep==='function'){value=rep.call(holder,key,value);}
+switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null';}
+gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==='[object Array]'){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null';}
+v=partial.length===0?'[]':gap?'[\n'+gap+partial.join(',\n'+gap)+'\n'+mind+']':'['+partial.join(',')+']';gap=mind;return v;}
+if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){k=rep[i];if(typeof k==='string'){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}else{for(k in value){if(Object.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}
+v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+mind+'}':'{'+partial.join(',')+'}';gap=mind;return v;}}
+if(typeof JSON.stringify!=='function'){JSON.stringify=function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' ';}}else if(typeof space==='string'){indent=space;}
+rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify');}
+return str('',{'':value});};}
+if(typeof JSON.parse!=='function'){JSON.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}
+return reviver.call(holder,key,value);}
+text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+
+('0000'+a.charCodeAt(0).toString(16)).slice(-4);});}
+if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}
+throw new SyntaxError('JSON.parse');};}}());
+
+// Precog core:
+var Precog = window.Precog || {};
+
+(function() {
+  var Util = {
+	  findScript: function(fragment) {
+      var scripts = document.getElementsByTagName('SCRIPT');
+
+      for (var i = 0; i < scripts.length; i++) {
+        var script = scripts[i];
+        var src = script.getAttribute('src');
+        if (src && ((typeof fragment == "string" && src.indexOf(fragment) != -1) || src.match(fragment))) {
+          return script;
+        }
+      }
+
+      return undefined;
+    },
+    getConfiguration: function() {
+      return Util.parseQueryParameters(Util.findScript(/precog[^\/.]*\.js/).getAttribute('src'));
+    },
+
+    parseQueryParameters: function(url) {
+      var index = url.indexOf('?');
+
+      if (index < 0) return {};
+
+      var query = url.substr(index + 1);
+
+      var keyValuePairs = query.split('&');
+
+      var parameters = {};
+
+      for (var i = 0; i < keyValuePairs.length; i++) {
+        var keyValuePair = keyValuePairs[i];
+
+        var split = keyValuePair.split('=');
+
+        var key = split[0];
+        var value = '';
+
+        if (split.length >= 2) {
+          value = decodeURIComponent(split[1]);
+        }
+
+        parameters[key] = value;
+      }
+
+      return parameters;
+    },
+
+    addQueryParameters: function(url, query) {
+      var suffix = url.indexOf('?') == -1 ? '?' : '&';
+
+      var queries = [];
+
+      for (var name in query) {
+        var value = (query[name] || '').toString();
+
+        queries.push(name + '=' + encodeURIComponent(value));
+      }
+
+      if (queries.length == 0) return url;
+      else return url + suffix + queries.join('&');
+    },
+
+    getConsole: function(enabled) {
+      var console = enabled ? window.console : undefined;
+      if (!console) {
+        console = {};
+
+        console.log   = function() {}
+        console.debug = function() {}
+        console.info  = function() {}
+        console.warn  = function() {}
+        console.error = function() {}
+      }
+
+      return console;
+    },
+
+    getProtocol: function() {
+      var src = Util.findScript(/precog[^\/.]*\.js/).getAttribute('src');
+      if(src && 'https:' == src.substr(0, 6))
+        return 'https:';
+      else
+        return 'http:';
+    },
+
+    createCallbacks: function(success, failure, msg) {
+      var successFn = function(fn, msg) {
+        if (fn) return fn;
+        else return function(result) {
+          if (result !== undefined) {
+            $.Log.debug('Success: ' + msg + ': ' + JSON.stringify(result));
+          }
+          else {
+            $.Log.debug('Success: ' + msg);
+          }
+        }
+      }
+
+      var failureFn = function(fn, msg) {
+        if (fn) return fn;
+        else return function(code, reason) {
+          $.Log.error('Failure: ' + msg + ': code = ' + code + ', reason = ' + reason);
+        }
+      }
+
+      return {
+        success: successFn(success, msg),
+        failure: failureFn(failure, msg)
+      };
+    },
+
+    removeTrailingSlash: function(path) {
+      if (path.length == 0) return path;
+      else if (path.substr(path.length - 1) == "/") return path.substr(0, path.length - 1);
+      else return path;
+    },
+
+    removeDuplicateSlashes: function(path) {
+      return path.replace(/[/]+/g, "/");
+    },
+
+    sanitizePath: function(path) {
+      if (path === undefined) throw Error("path cannot be undefined");
+      else return Util.removeDuplicateSlashes("/" + path + "/");
+    }
+  };
+
+  var Network = {
+    doAjaxRequest: function(options) {
+      var method   = options.method || 'GET';
+      var query    = options.query || {};
+      var path     = Util.addQueryParameters(options.path, query);
+      var content  = options.content;
+      var headers  = options.headers || {};
+      var success  = options.success;
+      var failure  = options.failure || function() {};
+
+      $.Log.info('HTTP ' + method + ' ' + path + ': headers(' + JSON.stringify(headers) + '), content('+ JSON.stringify(content) + ')');
+
+      var createNewXmlHttpRequest = function() {
+        if (window.XMLHttpRequest) {
+          return new XMLHttpRequest();
+        }
+        else {
+          return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+      }
+
+      var request = createNewXmlHttpRequest();
+
+      request.open(method, path);
+
+      request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          if (request.status == 200) {
+            if (request.responseText !== null && request.responseText.length > 0) {
+              success(JSON.parse(this.responseText));
+            }
+            else {
+              success(undefined);
+            }
+          }
+          else {
+            failure(request.status, request.statusText);
+          }
+        }
+      }
+
+      for (var name in headers) {
+        var value = headers[name];
+
+        request.setRequestHeader(name, value);
+      }
+
+      if (content !== undefined) {
+        request.setRequestHeader('Content-Type', 'application/json');
+
+        request.send(JSON.stringify(content));
+      }
+      else {
+        request.send(null);
+      }
+    },
+
+    doJsonpRequest: function(options) {
+      var method   = options.method || 'GET';
+      var query    = options.query || {};
+      var path     = Util.addQueryParameters(options.path, query);
+      var content  = options.content;
+      var headers  = options.headers || {};
+      var success  = options.success;
+      var failure  = options.failure || function() {};
+
+      $.Log.info('HTTP ' + method + ' ' + path + ': headers(' + JSON.stringify(headers) + '), content('+ JSON.stringify(content) + ')');
+
+      var random   = Math.floor(Math.random() * 214748363);
+      var funcName = 'PrecogJsonpCallback' + random.toString();
+
+      window[funcName] = function(content, meta) {
+        if (meta.status.code === 200) {
+          success(content);
+        }
+        else {
+          failure(meta.status.code, meta.status.reason);
+        }
+
+        document.head.removeChild(document.getElementById(funcName));
+        try{
+            delete window[funcName];
+        }catch(e){
+            window[funcName] = undefined;
+        }
+      }
+
+      var extraQuery = {};
+
+      extraQuery.method   = method;
+
+      for (_ in headers) { extraQuery.headers = JSON.stringify(headers); break; }
+
+      extraQuery.callback = funcName;
+
+      if (content !== undefined) {
+        extraQuery.content = JSON.stringify(content);
+      }
+
+      var fullUrl = Util.addQueryParameters(path, extraQuery);
+
+      var script = document.createElement('SCRIPT');
+
+      script.setAttribute('type', 'text/javascript');
+      script.setAttribute('src',  fullUrl);
+      script.setAttribute('id',   funcName);
+
+      // Workaround for document.head being undefined.
+      if (! document.head)
+        document.head = document.getElementsByTagName('head')[0];
+
+      document.head.appendChild(script);
+    },
+
+    createHttpInterface: function(doRequest) {
+      return {
+        get: function(path, callbacks, query, headers) {
+          doRequest(
+            {
+              method:   'GET',
+              path:     path,
+              headers:  headers,
+              success:  callbacks.success,
+              failure:  callbacks.failure,
+              query:    query
+            }
+          );
+        },
+
+        put: function(path, content, callbacks, query, headers) {
+          doRequest(
+            {
+              method:   'PUT',
+              path:     path,
+              content:  content,
+              headers:  headers,
+              success:  callbacks.success,
+              failure:  callbacks.failure,
+              query:    query
+            }
+          );
+        },
+
+        post: function(path, content, callbacks, query, headers) {
+          doRequest(
+            {
+              method:   'POST',
+              path:     path,
+              content:  content,
+              headers:  headers,
+              success:  callbacks.success,
+              failure:  callbacks.failure,
+              query:    query
+            }
+          );
+        },
+
+        remove: function(path, callbacks, query, headers) {
+          doRequest(
+            {
+              method:   'DELETE',
+              path:     path,
+              headers:  headers,
+              success:  callbacks.success,
+              failure:  callbacks.failure,
+              query:    query
+            }
+          );
+        }
+      }
+    }
+  }
+
+  Precog.$ = {};
+
+  var $ = Precog.$;
+
+  $.Util = Util;
+
+  $.Config = Util.getConfiguration();
+
+  $.Extend = function(object, extensions) {
+    for (var name in extensions) {
+      if (object[name] === undefined) {
+        object[name] = extensions[name];
+      }
+    }
+  }
+
+  $.Bool = function(v) {
+    return v === true || v === 1 || (v = (""+v).toLowerCase()) == "true" || v == "on" || v == "1";
+  }
+
+  $.Extend($.Config,
+    {
+      analyticsServer: Util.getProtocol() + "//demo.precog.io/services/quirrel/v1/",
+      useJsonp : "true",
+      enableLog : "false"
+    }
+  );
+
+  $.Config.analyticsServer = Util.removeTrailingSlash($.Config.analyticsServer);
+
+  $.Http = function() {
+    return $.Bool(Precog.$.Config.useJsonp) ? Precog.$.Http.Jsonp : Precog.$.Http.Ajax;
+  }
+
+  $.Http.Ajax  = Network.createHttpInterface(Network.doAjaxRequest);
+  $.Http.Jsonp = Network.createHttpInterface(Network.doJsonpRequest);
+
+  var console = Util.getConsole($.Bool($.Config.enableLog));
+
+  $.Log = {
+    log:    function(text) { console.log(text);   },
+    debug:  function(text) { console.debug(text); },
+    info:   function(text) { console.info(text);  },
+    warn:   function(text) { console.warn(text);  },
+    error:  function(text) { console.error(text); }
+  }
+
+  var http = $.Http();
+
+  Precog.quirrel = function(query, success, failure) {
+    var description = 'Quirrel query ' + query;
+
+    http.post(
+      $.Config.analyticsServer + "/query",
+      query,
+      Util.createCallbacks(success, failure, description),
+      {tokenId: $.Config.tokenId }
+    );
+  }
+
+  if("undefined" != typeof ReportGrid && "undefined" != typeof ReportGrid.query)
+  {
+    ReportGrid.query.quirrel = function(query) {
+      return ReportGrid.query.load(function(handler) {
+        Precog.quirrel(query, handler);
+      })
+    };
+  }
+})();
