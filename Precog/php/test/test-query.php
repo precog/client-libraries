52a060116544169f0d47ee292611eceb26a2d435
Alissa Pajer <alissa.pajer@gmail.com>
2012-04-09 16:06:26 -0600

    Added sleep(10) to wait for data to store before querying.

--- Precog/php/test/test-query.php
+++ Precog/php/test/test-query.php
@@ -14,11 +14,13 @@ class TestQuery extends PrecogBaseTest {
     {
         $path = TestQuery::setupPath();    
 
+        sleep(10);
+
         $value = $this->rg->query("
-            num := count(load(//unit_test/beta/test/php/query/TEST14471312814f8355686cdfc421311681)) 
+            num := count(load(/$path)) 
             a := 4 
             a + num");
-        echo "value is: " . $value[0] . "end";
+
         $this->assertIsA($value, "Array");
         $this->assertTrue($value[0] == 5);
     }
