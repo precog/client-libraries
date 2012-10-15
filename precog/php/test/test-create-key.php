a50686be1d5c5da096ab99b4940760a05a91fb72
Nathan <nathanlubchenco@Nathans-MacBook-Air.local>
2012-10-15 14:07:53 -0600

    create key test more robust (two grants instead of just one)

--- precog/php/test/test-create-key.php
+++ precog/php/test/test-create-key.php
@@ -5,7 +5,7 @@ require_once('basetest.php');
 class CreateAPIkeyTest extends PrecogBaseTest {
         function testCreateAPIkey() {
             $api = PrecogBaseTest::createApi($this->info);
-            $result = $api->createKey(array("grants"=>array(array("type"=>"read", "path"=>$this->info["path"]."foo/", "ownerAccountId"=> $this->info["accountId"], "expirationDate"=> null))));
+            $result = $api->createKey(array("grants"=>array(array("type"=>"read", "path"=>$this->info["path"]."foo/", "ownerAccountId"=> $this->info["accountId"], "expirationDate"=> null),array("type"=>"read", "path"=>$this->info["path"]."foo2/", "ownerAccountId"=> $this->info["accountId"], "expirationDate"=> null))));
             $this->assertTrue(isset($result) && strlen($result["apiKey"]) == 36);
         }
 }
