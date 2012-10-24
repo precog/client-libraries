6f81577b40f2844285af8b9399cc1919d39e7d62
gclaramunt <gabriel.claramunt@gmail.com>
2012-10-24 17:03:39 -0200

    [Finishes #38222907] Ingest Python client library and [Finishes #38222961] Query Python client library

---
+++ precog/python/precog/test/conftest.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2012 by ReportGrid, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# No portion of this Software shall be used in any application which does not
+# use the ReportGrid platform to provide some subset of its functionality.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from precog import precog
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--host", action="store", default="beta.precog.com", help="The hostname used for the unit test")
+    parser.addoption("--port", action="store", default=80, type=int, help="The port used for the unit test")
+    parser.addoption("--apiKey", action="store", default='A3BC1539-E8A9-4207-BB41-3036EC2C6E6D', help="The token used for the unit test")
+
+
