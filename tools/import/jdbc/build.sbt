50ae5a8d300ab80660d5126404a7c89a6c939f53
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2012-11-09 01:14:49 -0200

    added service call with dispatch

--- tools/import/jdbc/build.sbt
+++ tools/import/jdbc/build.sbt
@@ -3,3 +3,6 @@ name := "import-jdbc"
 version := "0.1"
 
 scalaVersion := "2.9.1"
+
+libraryDependencies += "net.databinder.dispatch" %% "dispatch-core" % "0.9.4"
+
--- tools/import/jdbc/src/main/scala/ImportJdbc.scala
+++ tools/import/jdbc/src/main/scala/ImportJdbc.scala
@@ -1,4 +1,5 @@
 import java.sql.{ Connection, DriverManager, ResultSet}
+import dispatch._
 
 //jdbc:mysql://localhost:3306/
 //admin admin
@@ -6,26 +7,51 @@ object ImportJdbc {
 
   def main(args:Array[String])= args match {
     
-		case Array(url,user,password) => importJdbc(url,user,password)
-		case _ => error ("Incorrect number of parameters, required: url user password")
+		case Array(dbUrl,user,password) => importJdbc(dbUrl,user,password)
+		case _ => error ("Incorrect number of parameters, required: database url user password")
 	}
 
-	def importJdbc(url:String, user:String, password:String) = {
-		val conn= DriverManager.getConnection(url,user,password)
+	def importJdbc(dbUrl:String, user:String, password:String) = {
+		val conn= DriverManager.getConnection(dbUrl,user,password)
 		val metadata= conn.getMetaData()
 		println("Catalogs:")
 		val catalogs = oneColumnRs(metadata.getCatalogs()).toArray;
 		println(present(catalogs.toArray))
 		println("Select a catalog")
-		val cat=catalogs(readInt-1)
-		val tables = tablesList(metadata.getTables(cat,null,null,null)).toArray;
+		val cat=catalogs(readInt()-1)
+
+		val tables = tablesList(metadata.getTables(cat,null,null,Array("TABLE"))).toArray;
 		println("Tables:")
 		println(present(tables.toArray))
+
 		println("Select a table")
-		val tab=tables(readInt-1)
-		println("Table: %s".format(tab))
-		val table=tableDesc(metadata.getColumns(cat,null,tab,null))
-		println(table.mkString(" | "))
+		val tabName=tables(readInt()-1)
+		println("Table: %s".format(tabName))
+		
+		//use a prepared staemtent to get the metadata
+		val stmt = conn.prepareStatement("select * form %s".format(tabName));
+		val tblMetaData = stmt.getMetaData()
+		val count=tblMetaData.getColumnCount();
+        //column selection
+
+
+        // use api key and dispatch to call ingest
+        println("ingestion host")
+        val host=readLine()
+        println("API KEY for ingestion")
+        val apiKey=readLine()
+        println("ingestion path ( /{userId}/....)")
+        val path=readLine()
+
+        //consider lazyness
+        val rs = stmt.executeQuery();
+        val data= rsIterator(rs)( row => for (i <- 1 to count) yield row.getString(i))
+        val body= data.map( _.mkString(",")).mkString("\n")
+        val serverUrl= url("%s/ingest/v1/sync/fs/%s".format(host,path))
+        var serverCall= serverUrl << body <<? Map("apiKey" -> apiKey) 
+         
+        
+
 	}
 
 	def rsIterator[T](rs:ResultSet)(f:ResultSet => T) = new Iterator[T] {
