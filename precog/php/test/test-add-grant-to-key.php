2c50a7ab168436d4eff5555dc84f1fcae9889955
gclaramunt <gabriel.claramunt@gmail.com>
2012-12-19 17:58:47 -0200

    unit test updates to accomodate new grant schema [Finishes #40652055]

--- precog/php/test/test-add-grant-to-key.php
+++ precog/php/test/test-add-grant-to-key.php
@@ -5,11 +5,21 @@ require_once('basetest.php');
 class AddGrantToKeyTest extends PrecogBaseTest {
     function testAddGrantToKey() {
         $api = PrecogBaseTest::createApi($this->info);
-        $result = $api->createKey(array("grants"=>array(array("type"=>"read", "path"=>$this->info["path"]."foo/", "ownerAccountId"=> $this->info["accountId"], "expirationDate"=> null))));
+
+		$grant= array("name"=>"php-test","description"=>"",
+    	"grants"=>array(array(
+    			"parentIds"=> array(), 
+    			"expirationDate"=> null,
+    			"permissions"=>array(array("accessType"=>"read", "path"=>$this->info["path"]."foo/","ownerAccountId"=> $this->info["accountId"]))
+    		))
+    	);
+
+        $result = $api->createKey($grant);
  		$apiKey1 = $result["apiKey"];
-        $result = $api->createKey(array("grants"=>array(array("type"=>"write", "path"=>$this->info["path"]."foo/", "ownerAccountId"=> $this->info["accountId"], "expirationDate"=> null))));
+        $result = $api->createKey($grant);
  	    $apiKey2 = $result["apiKey"];
     	$result = $api->describeKey($apiKey2);
+
  		$grantId = $result["grants"][0]["grantId"];
  		$api->addGrantToKey($apiKey1, $grantId);
         $result = $api->describeKey($apiKey1);
