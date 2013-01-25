8885c26ac0d3a9399a649a54c8e397e59fe57337
Daniel C. Sobral <dcsobral@precog.com>
2013-01-25 16:15:27 -0500

    Ignore v1 test

--- precog/java/src/test/java/com/precog/api/ClientTest.java
+++ precog/java/src/test/java/com/precog/api/ClientTest.java
@@ -14,6 +14,7 @@ import com.precog.json.gson.GsonToJson;
 import com.precog.json.gson.RawJson;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Ignore;
 
 import java.io.IOException;import java.util.Map;
 
@@ -211,6 +212,7 @@ public class ClientTest {
     }
 
     @Test
+    @Ignore
     public void testQuery() throws IOException {
         //just test the query was sent and executed successfully
 
